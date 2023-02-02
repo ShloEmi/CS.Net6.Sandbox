13873bd356da3cdb1adaef3d312edade16868787
Barefoot <shlomi_o@hotmail.com>
2023-02-02 09:06:46 +0000

    + ReflectionTests.cs

---
+++ CSNet6Sandbox/CSNet6Sandbox/Reflection/ReflectionTests.cs
@@ -0,0 +1,11 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSNet6Sandbox.Reflection;
+
+public class ReflectionTests
+{
+    [Test]
+    public void TBD1() => 
+        typeof(object).IsPrimitive.Should().BeFalse();
+}
